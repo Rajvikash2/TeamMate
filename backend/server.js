5cf7c40289882a34dea73027c7f9b79a27ee9b6a
Rajvikash <rajvikash.r2022cse@sece.ac.in>
2025-02-03 19:25:23 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method);
+    next();
+})
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log("conected to db & server is running on port",process.env.PORT)
+    })
+})
+.catch((error)=>{
+    console.log("Failed to connect to db",error);
+})
